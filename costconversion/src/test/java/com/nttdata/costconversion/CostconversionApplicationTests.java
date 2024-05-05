76623b6e0323c213fd13249840411ebe8903c4f0
christian <51129408+chdomenech@users.noreply.github.com>
2024-05-05 11:54:31 -0500

    Cambios
    Cambios

--- costconversion/src/test/java/com/nttdata/costconversion/CostconversionApplicationTests.java
+++ costconversion/src/test/java/com/nttdata/costconversion/CostconversionApplicationTests.java
@@ -1,9 +1,7 @@
 package com.nttdata.costconversion;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CostconversionApplicationTests {
 
 	@Test
